0f833fcf564abdbb2ddd0966fd7bc56ba0d4582b
Faaiz <fmhs9009@gmail.com>
2025-05-27 18:44:56 +0530

    HOD, Student, Admin models created

---
+++ ServerSide/Model/HOD.js
@@ -0,0 +1,42 @@
+// models/HOD.js
+import mongoose from 'mongoose';
+
+const hodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+    required: true,
+  },
+  empId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  department: {
+    type: String,
+    required: true,
+    unique: true, // har dept. ka ek hi HOD hota hai
+  },
+  departmentFaculty: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Faculty',
+    },
+  ],
+}, { timestamps: true });
+
+const HOD = mongoose.model('HOD', hodSchema);
+export default HOD;
