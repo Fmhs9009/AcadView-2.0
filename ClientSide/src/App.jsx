262925757e0b5e326654767788374f6c600a9377
Faaiz <fmhs9009@gmail.com>
2025-07-28 15:34:27 +0530

    localstorage token commented

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem('role');
-    localStorage.removeItem('token');
+    // localStorage.removeItem('token');
     Cookies.remove('auth_token');
     navigate('/');
   };
